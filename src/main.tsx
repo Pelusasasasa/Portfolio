4b05ade40894276631b2cae23884ca4ff2bcc711
Pelusa <agustinlorenzatto@gmail.com>
2025-07-21 12:04:51 -0300

    Agregando Vercel Web Analitics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Home } from './pages/Home'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Home/>
+    <Analytics/>
   </StrictMode>,
 )
